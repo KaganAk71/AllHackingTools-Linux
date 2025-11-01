667717dbc37ceb16d3b936037225cb73e20b5464
KaganAk71 <177409913+KaganAk71@users.noreply.github.com>
2025-11-01 21:40:02 +0300

    Update Install.sh
    Added sudo control and changed setup structure.

--- Install.sh
+++ Install.sh
@@ -8,16 +8,19 @@ yellow='\e[0;33m'
 orange='\e[38;5;166m'
 green='\033[92m'
 
+if pgrep -s 0 '^sudo$' > /dev/null ; then
+    echo''
+else
+    echo 'Please run with sudo!'
+fi
+
 clear
 sleep 1.5
 echo -e "$default"
-apt install python
-apt install python2 
-apt install pip
-apt install pip2
-pip install requests
-pip2 install requests
+sudo apt install python3
+sudo apt install pip
+sudo pip install requests
 cd
 cd
 cd AllHackingTools
-python2 src/InstallMenu.py
+python3 src/InstallMenu.py
